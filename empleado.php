82c12d5168c3557db453430077ab864cd8eac20c
wjzapata <83246650+wjzapata@users.noreply.github.com>
2021-10-21 04:02:55 -0500

    Create empleado.php

---
+++ empleado.php
@@ -0,0 +1,69 @@
+<?php
+
+class Empleado{
+
+    public $id;
+    public $nombre;
+    public $correo;
+
+    public function __construct($id,$nombre,$correo) {
+        $this->id=$id;
+        $this->nombre=$nombre;
+        $this->correo=$correo;
+
+    }
+
+    public static function consultar(){
+        $listaEmpleados=[];
+        $conexionBD=BD::crearInstancia();
+        $sql= $conexionBD->query("SELECT * FROM empleados");
+
+        foreach($sql->fetchAll() as $empleado){
+
+            $listaEmpleados[]= new Empleado($empleado['id'], $empleado['nombre'],$empleado['correo']);
+
+
+        }
+
+        return $listaEmpleados;
+    }
+
+
+    public static function crear($nombre, $correo){
+
+        $conexionBD=BD::crearInstancia();
+
+        $sql= $conexionBD->prepare("INSERT INTO empleados(nombre, correo) VALUES(?,?)");
+        $sql->execute(array($nombre,$correo));
+
+    }
+    public static function borrar($id){
+        $conexionBD=BD::crearInstancia();
+
+        $sql= $conexionBD->prepare(" DELETE FROM empleados WHERE id=?");
+        $sql->execute(array($id));
+
+    }
+    public static function buscar($id){
+
+        $conexionBD=BD::crearInstancia();
+
+        $sql= $conexionBD->prepare(" SELECT * FROM empleados WHERE id=?");
+        $sql->execute(array($id));
+        $empleado=$sql->fetch();
+        return new Empleado($empleado['id'], $empleado['nombre'], $empleado['correo']);
+
+    }
+    public static function editar($nombre,$correo,$id){
+
+        $conexionBD=BD::crearInstancia();
+        $sql= $conexionBD->prepare("UPDATE empleados SET nombre=?, correo=? WHERE id=? ");
+        $sql->execute(array($nombre,$correo,$id));
+
+    }
+
+}
+
+
+
+?>
