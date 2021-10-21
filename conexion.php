83eceb8b68f715abad4c1888cbab0a5873eb80cc
wjzapata <83246650+wjzapata@users.noreply.github.com>
2021-10-21 04:04:13 -0500

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,19 @@
+<?php
+
+class BD{
+    private static $instancia=NULL;
+
+    public static function crearInstancia() {
+
+        if(!isset( self::$instancia)){
+
+            $opcionesPDO[PDO::ATTR_ERRMODE]= PDO::ERRMODE_EXCEPTION;
+            self::$instancia= new PDO('mysql:host=localhost;dbname=empleados','root','',$opcionesPDO);
+            
+        }
+        return self::$instancia;
+
+    }
+}
+
+?>
---
+++ ruteador.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+
+include_once("controladores/controlador_".$controlador.".php");
+
+$objControlador="Controlador".ucfirst($controlador);
+
+$controlador= new $objControlador();
+
+$controlador->$accion();
+
+
+?>
